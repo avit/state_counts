6d8519060f6f40c89cd42b9f3e7e2c5eac371ef9
Andrew Vit <andrew@avit.ca>
2014-05-23 17:56:29 -0700

    Example 2

---
+++ spec/examples/2_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require_relative '../../implementation.rb'
+
+describe "Example 2" do
+
+  let(:example) {
+    [
+      double(state: "BC",      country: "Canada",        count: 5),
+      double(state: "Ontario", country: "CA",            count: 15),
+      double(state: "QC",      country: nil,             count: 25),
+      double(state: "Oregon",  country: "USA",           count: 35),
+      double(state: "WA",      country: "United States", count: 45),
+      double(state: "CA",      country: nil,             count: 55),
+      double(state: "WA",      country: "Australia",     count: 65)
+    ]
+  }
+
+  it "counts states" do
+    expect( state_counts(example) ).to be == {
+      CA_BC => 5,
+      CA_ON => 15,
+      CA_QC => 25,
+      US_OR => 35,
+      US_WA => 45,
+      US_CA => 55,
+      AU_WA => 65
+    }
+  end
+
+end
